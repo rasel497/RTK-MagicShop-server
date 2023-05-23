b68e918ae026ff26ab2cba97c879e4543463c56a
rasel497 <rasel.csediu44@gmail.com>
2023-05-23 11:50:54 +0600

    test

--- src/store/Store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import userAuthSlice from "../component/State/userAuthSlice";
-
-export const store = configureStore({
-    reducer: {
-        users: userAuthSlice,
-    }
-});
